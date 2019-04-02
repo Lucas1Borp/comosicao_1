55e1a51e6f13badf152ac33ad9ba8654dbde8c29
Lucas Borges <lucas.a.b.p1@gmail.com>
2019-04-02 00:02:29 -0300

    Implementando a classe Colaborador

--- src/entidades/Colaborador.java
+++ src/entidades/Colaborador.java
@@ -1,5 +1,71 @@
 package entidades;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import entidades.enums.ClasseColaborador;
+
 public class Colaborador {
+
+	private String nome;
+	private ClasseColaborador classeColaborador;
+	private Double salarioBase;
+
+	private Departamento departamento;
+	private List<HoraContrato> contratos = new ArrayList<>();
+
+	public Colaborador() {
+
+	}
+
+	public Colaborador(String nome, ClasseColaborador classeColaborador, Double salarioBase,
+			Departamento departamento) {
+		this.nome = nome;
+		this.classeColaborador = classeColaborador;
+		this.salarioBase = salarioBase;
+		this.departamento = departamento;
+
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public ClasseColaborador getClasseColaborador() {
+		return classeColaborador;
+	}
+
+	public void setClasseColaborador(ClasseColaborador classeColaborador) {
+		this.classeColaborador = classeColaborador;
+	}
+
+	public Double getSalarioBase() {
+		return salarioBase;
+	}
+
+	public void setSalarioBase(Double salarioBase) {
+		this.salarioBase = salarioBase;
+	}
+
+	public Departamento getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(Departamento departamento) {
+		this.departamento = departamento;
+	}
+
+	public List<HoraContrato> getContratos() {
+		return contratos;
+	}
+
+	public void setContratos(List<HoraContrato> contratos) {
+		this.contratos = contratos;
+	}
 
 }
